d6f0b25409451eca11af970d2e9429b201423389
pangboww <pangboww@gmail.com>
2015-12-15 16:36:53 +0100

    update sensor

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,21 +1,21 @@
 int sensorPin = A0;
-int speakerPin = 10;
+int speakerPin = 8;
 int sensorValue = 400; 
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(sensorPin, INPUT);
   pinMode(speakerPin, OUTPUT);
-  Serial.begin(9600);
+//  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int light= analogRead(sensorPin);
   if (light > sensorValue){
-    analogWrite(speakerPin, 200);
+    digitalWrite(speakerPin, HIGH);
   } else {
-    analogWrite(speakerPin, 0);
+    digitalWrite(speakerPin, LOW);
   }
-  Serial.println(light);
+//  Serial.println(light);
 }
